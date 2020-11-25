2163a7860e0419be590a9e9af8932d79ce99ecfc
Ariel Yanarico <Ariel.Yanarico@tutanota.com>
2020-11-25 01:08:49 -0400

    Added logout function

--- src/reducers/session.js
+++ src/reducers/session.js
@@ -1,9 +1,12 @@
-import { UPDATE_SESSION } from '../actions/session';
+import { UPDATE_SESSION, REMOVE_SESSION } from '../actions/session';
 
 export const session = (state = false, action) => {
   switch (action.type) {
     case UPDATE_SESSION:
       return action.session;
+
+    case REMOVE_SESSION:
+      return false;
 
     default:
       return state;
